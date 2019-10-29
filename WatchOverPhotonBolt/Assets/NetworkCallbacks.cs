77f747e74da52ac4cbd1af399ccf9a1193d8e495
Tristan <tristan.schorr@hotmail.fr>
2019-10-29 20:47:44 +0100

    setup photon connection

---
+++ WatchOverPhotonBolt/Assets/NetworkCallbacks.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string scene)
+    {
+        var spawnPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+
+        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
